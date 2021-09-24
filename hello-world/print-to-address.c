ab467174e8d7147553f48b5a3231333cb1709681
Cedrik Jiruse <github.meow@gmail.com>
2021-09-24 12:26:10 +0100

    added print-to-address

---
+++ hello-world/print-to-address.c
@@ -0,0 +1,27 @@
+/* program to print an address */ 
+
+#include <stdio.h> 
+
+ 
+
+int main(void) 
+
+{ 
+
+    //  printf("ABC Corp"); 
+
+    //  printf("\n"); 
+
+    //  printf("Tallaght"); 
+
+    //  printf("\n"); 
+
+    //  printf("Dublin24"); 
+
+    //  printf("\n"); 
+
+ 
+
+     return 0; 
+
+} 
