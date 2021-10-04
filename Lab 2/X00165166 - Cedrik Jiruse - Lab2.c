c4f35298788bdf3a3bee308332de8d641b8fac3e
Cedrik Jiruse <github.meow@gmail.com>
2021-10-04 19:43:38 +0100

    changed sphere volume calculation into a function

--- Lab 2/X00165166 - Cedrik Jiruse - Lab2.c
+++ Lab 2/X00165166 - Cedrik Jiruse - Lab2.c
@@ -4,33 +4,16 @@
 // the dimensions are defined by the user
 
 const float pi= 3.14159265359;
+float sphereVolume(float r);
 
 int main(void)
 {
 
 	// VOLUME OF A SPHERE
 	// variables are declared here
-	float sphereRadius, sphereVolume; // declare variables for this shape
-	int badAttempts = 0;
+	float radius;
 
-	do {
-		// get the data from user and store it in sphereRadius
-		// do while loop is used to catch bad data from the user
-		printf("Please input a positive number for the radius of the circle: ");
-		scanf("%f", &sphereRadius);
-
-		// count the number of bad attempts at inputing the correct value
-		// stop the program after 3 failed attempts and print an error message
-		badAttempts = badAttempts + 1;
-		if (badAttempts == 3) {
-			printf("Program closing due to 3 failed attempts at inputing data.");
-			return 0;
-		}
-	} while (sphereRadius <= 0);
-	
-	// calculate volume of a sphere using vaules from the user
-	sphereVolume = (4.0f/3.0f) * pi * (sphereRadius * sphereRadius * sphereRadius);	// (4.0f/3.0f) is used instead of (4/3) because the latter gave an integer output
-	printf("Volume of sphere is %f\n", sphereVolume);
+	printf("Volume of sphere is %f\n", sphereVolume(radius));
 
 
 	// VOLUME OF COMPLEX SHAPE
@@ -113,4 +96,13 @@ int main(void)
 	printf("\nVolume of shape %f\n", (triangleVolume + cuboidVolume + secondCuboidVolume));
 
 	return 0;
+
+	float sphereVolume(float r) {
+		// calculates the volume of a sphere
+
+		float volume = (4.0f/3.0f) * pi * (r * r * r);	
+		// (4.0f/3.0f) is used instead of (4/3) because the latter gave an integer output
+
+		return volume; // returns the calculated output
+	}
 }
