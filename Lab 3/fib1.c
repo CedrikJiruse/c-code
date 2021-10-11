70126c10d7275f6b7fe1eff22f779c39af30e2c4
Cedrik Jiruse <github.meow@gmail.com>
2021-10-11 17:54:57 +0100

    initial commit

--- Lab 3/fib1.c
+++ Lab 3/fib1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int isFibonacci(int userFib);
+
 int main() {
 
     int inputFib;
@@ -7,8 +9,24 @@ int main() {
     printf("Enter Number: ");
     scanf("%d", &inputFib);
 
+    if (inputFib == isFibonacci(inputFib)) {
+        printf("Number is Fibonacci");
+    }
+    else {
+        printf("Number is not Fibonacci");
+    }
+    
 }
 
 int isFibonacci(int userFib) {
+    int currentFib = 0, nextFib = 1, x;
+
+    while (userFib > currentFib)
+    {
+        x = nextFib;
+        nextFib = currentFib + nextFib;
+        currentFib = x;
+    }
     
+    return currentFib;
 }
