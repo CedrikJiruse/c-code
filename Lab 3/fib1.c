c526ac420ea3dc648a8b74a75c8ff43dd0dbb5f7
Cedrik Jiruse <github.meow@gmail.com>
2021-10-19 18:26:06 +0100

    added comments and cleaned up code

--- Lab 3/fib1.c
+++ Lab 3/fib1.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 
+// declare functions
 int isFibonacci(int userFib);
 
 int main() {
-    
+
+    // declare variables
     int inputFib;
 
+    // input number from user
     printf("Enter Number: ");
     scanf("%d", &inputFib);
 
+    // checks if input is positive
     while (inputFib < 0) {
         printf("Number must be positive. Enter again: ");
         scanf("%d", &inputFib);
     }
-    
 
+    // checks if input is equal to any number in the fibonacci sequence
+    // prints a message accordingly
     if (inputFib == isFibonacci(inputFib)) {
-        printf("Number is Fibonacci\n");
+        printf("Number is Fibonacci\n\n");
     }
     else {
-        printf("Number is not Fibonacci\n");
+        printf("Number is not Fibonacci\n\n");
     }
 }
 
 int isFibonacci(int userFib) {
+    // runs the fibonacci sequence until it is greater than or equal to the input
+
     int currentFib = 0, nextFib = 1, x;
 
-    while (userFib > currentFib)
-    {
+    while (userFib > currentFib) {
         x = nextFib;
         nextFib = currentFib + nextFib;
         currentFib = x;
--- Lab 3/fib2.c
+++ Lab 3/fib2.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
 
+// declare functions
 int isFibonacci(int userFib);
 
+// declare global variables
+int counter = 0;
+
 int main() {
-    
-    int inputFib;
 
+    // declare variables
+    int firstInputFib, secondInputFib;
+
+    // input number from user
     printf("Enter Number: ");
-    scanf("%d", &inputFib);
+    scanf("%d%d", &firstInputFib, &secondInputFib);
 
-    while (inputFib < 0) {
+    // checks if input is positive
+    while (firstInputFib < 0) {
         printf("Number must be positive. Enter again: ");
-        scanf("%d", &inputFib);
+        scanf("%d%d", &firstInputFib, &secondInputFib);
     }
-    
-    if (inputFib == isFibonacci(inputFib)) {
-        printf("Number is Fibonacci\n");
+
+    // checks if input is equal to any number in the fibonacci sequence
+    // prints a message accordingly
+    if (firstInputFib == isFibonacci(firstInputFib)) {
+        printf("Number is Fibonacci\n\n");
     }
     else {
-        printf("Number is not Fibonacci\n");
+        printf("Number is not Fibonacci\n\n");
     }
 }
 
 int isFibonacci(int userFib) {
-    int currentFib = 0, nextFib = 1, x;
+    // runs the fibonacci sequence until it is greater than or equal to the input
 
-    while (userFib > currentFib)
-    {
+    int currentFib = 0, nextFib = 1, x; // used to compute the fibonacci sequence
+
+    while (userFib > currentFib) {
+
         x = nextFib;
         nextFib = currentFib + nextFib;
         currentFib = x;
+
+        counter += 1;
+        printf("%d %d\n", counter, currentFib);
     }
 
     return currentFib;
