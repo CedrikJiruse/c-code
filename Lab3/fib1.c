e75f75e8e191f8347a32a6159f92298a6fcd4a40
Cedrik Jiruse <github.meow@gmail.com>
2021-10-25 14:55:56 +0100

    removed inputData for cleaner code

--- Lab3/fib1.c
+++ Lab3/fib1.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// declare functions
-void inputData(int* fib);
-
 int main() {
 
 	// declare variables
@@ -11,7 +8,15 @@ int main() {
 
 	// input number from user
 	printf("Enter Number: ");
-	inputData(&inputFib);
+	scanf("%d", &inputFib);
+
+	// check if the input is valid
+	if (inputFib <= 0 )
+	{
+		printf("Number must be positive.\n");
+		exit(0);
+	}
+	
 
 	// runs the fibonacci sequence until it is greater than or equal to the input
 	while (inputFib > currentFib) {
@@ -29,27 +34,4 @@ int main() {
 	else {
 		printf("Number is not Fibonacci\n\n");
 	}
-}
-
-void inputData(int* input) {
-
-	int badAttempts = 0;
-
-	do {
-
-		if (badAttempts == 3) {
-			printf("\nToo many failed attempts at inputing data. Program closing.\n");
-			exit(0);
-		}
-
-		scanf("%d", &*input);
-
-		if (*input <= 0) {
-			printf("Incorrect data. Must be a positive number. ");
-			badAttempts += 1;
-		}
-		else {
-			return;
-		}
-	} while (badAttempts < 3);
 }
