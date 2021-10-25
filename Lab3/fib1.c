a866b3c5d41969492ad429a46bade1f0449a2e1a
Cedrik Jiruse <github.meow@gmail.com>
2021-10-25 18:56:36 +0100

    fixed a bug where it would not display the first number

--- Lab3/fib1.c
+++ Lab3/fib1.c
@@ -20,7 +20,7 @@ int main() {
 	while (inputFib > currentFib) {
 
 		x = nextFib;
-		nextFib = currentFib + nextFib;
+		nextFib += currentFib;
 		currentFib = x;
 	}
 
--- Lab3/fib2.c
+++ Lab3/fib2.c
@@ -4,39 +4,39 @@
 int main() {
 
 	// declare variables
-	int firstInputFib, secondInputFib;
+	int smallerInputFib, biggerInputFib;
 
 	// input number from user
 	printf("Enter Numbers: ");
-	scanf("%d%d", &firstInputFib, &secondInputFib);
+	scanf("%d%d", &smallerInputFib, &biggerInputFib);
 
 	// check if the input is valid
-	if (firstInputFib < 0 || secondInputFib <= 0) {
-		printf("Numbers must be positive.\n\n");
+	if (smallerInputFib < 0) {
+		printf("Number must be positive.\n\n");
 		exit(0);
 	}
 
 	// check if first is smaller then second
-	if (firstInputFib >= secondInputFib) {
+	if (smallerInputFib >= biggerInputFib) {
 		printf("Second number must be bigger than the first.\n\n");
 		exit(0);
 	}
-	
 
-	// runs the fibonacci sequence until it is greater than or equal to the input
-	int currentFib = 0, nextFib = 1, x;
+	// runs the fibonacci sequence until it is greater than or equal to the second input
+	int currentFib = 0, x = 0, nextFib = 1;
 
-	for (int i = firstInputFib; i < secondInputFib; i += 1) {
+	for (int i = smallerInputFib; currentFib <= biggerInputFib; i += 1) {
 
-		while (i > currentFib) {
+		while (i >= currentFib) {
 
-			x = nextFib;
-			nextFib = currentFib + nextFib;
-			currentFib = x;
-
-			if (firstInputFib <= currentFib && currentFib <= secondInputFib) {
+			// only prints numbers between the range given
+			if (smallerInputFib <= currentFib) {
 				printf("%d\n", currentFib);
 			}
+
+			x = nextFib;
+			nextFib += currentFib;
+			currentFib = x;
 		}
 	}
 }
